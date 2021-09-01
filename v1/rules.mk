b481005769fc83097b030a3a2c44dd991ff5cb39
hsaito18 <hsaito2002@gmail.com>
2021-08-31 23:41:49 -0700

    added v1 kb firmware

---
+++ v1/rules.mk
@@ -0,0 +1,17 @@
+# MCU name
+MCU = at90usb1286
+
+# Bootloader selection
+#   Teensy       halfkay
+#   Pro Micro    caterina
+#   Atmel DFU    atmel-dfu
+#   LUFA DFU     lufa-dfu
+#   QMK DFU      qmk-dfu
+#   ATmega32A    bootloadHID
+#   ATmega328P   USBasp
+BOOTLOADER = atmel-dfu
+
+RGBLIGHT_ENABLE = no
+BACKLIGHT_ENABLE = no # Enable keyboard backlight functionality
+
+LTO_ENABLE = yes
